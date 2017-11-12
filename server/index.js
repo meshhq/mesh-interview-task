ba7c57ef54b57ea00f576a90ff6e3d830d1804e4
Leonardo J. Perez <find.leonardo@gmail.com>
2017-11-12 18:19:07 -0500

    Consolidated modules for cleaner implementation.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
+'use strict';
+
+const Github = require('../github');
+
+const connection = {
+    port: 3010,
+    host: 'localhost'
+};
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/githubPayload',
+        handler: function (req, reply) {
+            const user = req.payload.user; // add selectn
+            Github.getUserInfo(user, (data) => {
+                reply(data);
+            });
+        }
+    }
+];
+
 module.exports = {
-    connection: require('./connection'),
-    routes: require('./routes')
-}
+    connection,
+    routes
+};
